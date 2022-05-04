1c8342bb74a9f425289a0ba2ab90c5eb5b4fdcb7
izacode <izaelec@gmail.com>
2022-05-04 16:04:56 -0400

    auth type check addded

--- src/middleware/auth-middleware.ts
+++ src/middleware/auth-middleware.ts
@@ -2,15 +2,12 @@ import { Request, Response, NextFunction } from "express";
 
 export const checkCredentials = (req: Request, res: Response, next: NextFunction) => {
   if (!req.headers.authorization) return res.sendStatus(401);
-  //   console.log(req.headers.authorization);
+  const authorizationType = req.headers.authorization.split(" ")[0];
   const authorization = req.headers.authorization.split(" ")[1];
-
   const buff = Buffer.from(authorization, "base64");
   const decodedAuth = buff.toString("utf-8");
   const login = decodedAuth.split(":")[0];
   const password = decodedAuth.split(":")[1];
-  //   console.log(login);
-  //   console.log(password);
-  if (login !== "admin" || password !== "qwerty") return res.sendStatus(401);
+  if (login !== "admin" || password !== "qwerty" || authorizationType !=="Basic") return res.sendStatus(401);
   next();
 };
